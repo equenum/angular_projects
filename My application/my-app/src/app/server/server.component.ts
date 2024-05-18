2993aeb8833d316412d186ffa53f94a07d4c6265
Egor Quenum <georgequenum@yahoo.com>
2024-05-19 00:38:45 +0400

    Add usagr of ngStyle ans ngClass directives

--- My application/my-app/src/app/server/server.component.ts
+++ My application/my-app/src/app/server/server.component.ts
@@ -3,15 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server', // unique html selector, usually starts with 'app-'
   templateUrl: './server.component.html',
+  styleUrl: './server.component.css',
 })
 export class ServerComponent {
   // properties can be used in string interpolation data binding with the '{{ }}' syntax.
   // Inside of '{{ }}' we can put any expression that can be resolved in a string.
   // No multiline, loops or flow control statements though, only ternary is allowed.
   serverId = 123;
-  status = 'Online';
+  status = '';
+
+  constructor() {
+    this.status = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
 
   getServerStatus() {
     return this.status;
+  }
+
+  getBgColor() {
+    return this.status == 'Online' ? 'green' : 'red';
   }
 }
