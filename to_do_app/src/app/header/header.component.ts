5da4b0753c3406faba5d85593bef7fc07460fe2c
Egor Quenum <georgequenum@yahoo.com>
2024-07-28 21:49:25 +0400

    Add header component

---
+++ to_do_app/src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header', // a good practice is to have at least two word selectors
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
