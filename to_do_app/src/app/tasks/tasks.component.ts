fc8d7cc1b2cd065315f703c0b2187dcdf0e22e17
Egor Quenum <georgequenum@yahoo.com>
2024-08-04 22:33:49 +0400

    Implement the logic to ass new tasks

--- to_do_app/src/app/tasks/tasks.component.ts
+++ to_do_app/src/app/tasks/tasks.component.ts
@@ -1,12 +1,15 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
-import { Task } from './task/task.model';
 import { DUMMY_TASKS } from '../data/dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
+
+import type { Task } from './task/task.model';
+import type { TaskData } from './new-task/new-task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -14,6 +17,7 @@ export class TasksComponent {
   @Input({ required: true }) selectedUserId!: string;
   @Input({ required: true }) userName!: string;
   tasks: Task[] = DUMMY_TASKS as Task[];
+  isAddingTask: boolean = false;
 
   get selectedUserTasks(): Task[] {
     return this.tasks.filter((task) => task.userId === this.selectedUserId);
@@ -24,5 +28,26 @@ export class TasksComponent {
     if (taskIndex !== -1) {
       this.tasks.splice(taskIndex, 1);
     }
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onAddTaskCanceled() {
+    this.isAddingTask = false;
+  }
+
+  onTaskAdded(taskData: TaskData) {
+    const newTask: Task = {
+      id: `t${this.tasks.length + 1}`,
+      userId: this.selectedUserId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate,
+    };
+
+    this.tasks.push(newTask);
+    this.isAddingTask = false;
   }
 }
